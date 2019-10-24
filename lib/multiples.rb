bda8f8531e21ceb54fb84e75162305f6907e5281
DannyBrito <britodanny1997@gmail.com>
2019-10-24 18:41:06 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+
+def collect_multiples(num)
+n_arr =(1...num).to_a.select{|num| num%3 == 0 || num%5 == 0}
+end
+
+def sum_multiples(num)
+    collect_multiples(num).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        n_arr =(1...self.limit).to_a.select{|num| num%3 == 0 || num%5 == 0}
+    end
+        
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+
+end
